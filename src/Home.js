8e9b17e56693eebdc2abf2ffe3e1864fc26c8d94
Matthew Epps <mepps32@gmail.com>
2022-01-08 19:34:29 -0800

    add firebase config and change title on home page

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Home = () => (
   <div className="flex flex-col">
-    <h1>Welcome home!</h1>
+    <h1>Welcome to INFL!</h1>
     <Link to="/auctions/create">Create auction</Link>
     <Link to="/auctions/0x1">Auction #1</Link>
   </div>
